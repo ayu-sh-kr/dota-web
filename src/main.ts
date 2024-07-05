06cccc7146ca67ebeea80f341fa8dab3e5af8a56
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-05 22:02:00 +0530

    Update main.ts: define tagname map for newly created components

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import {OfferComponent} from "@dota/components/home/offer.component.ts";
 import {HeroSectionComponent} from "@dota/components/home/hero-section.component.ts";
 import {HeaderComponent} from "@dota/components/home/header.component.ts";
 import '@dota/pages/home.page'
+import {NotificationHolderComponent} from "@dota/components/utils/notification/notification-holder.component.ts";
+import {NotificationComponent} from "@dota/components/utils/notification/notification.component.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <home-page></home-page>
@@ -13,6 +15,8 @@ declare global {
     interface HTMLTagNameMap {
         'app-offer': OfferComponent,
         'app-header': HeaderComponent,
-        'app-hero': HeroSectionComponent
+        'app-hero': HeroSectionComponent,
+        'notification-holder': NotificationHolderComponent
+        'app-notification': NotificationComponent
     }
 }

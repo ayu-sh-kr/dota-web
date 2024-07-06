6eb028f9401895df0efcbbd94dd38be05f8258a4
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-07 01:09:09 +0530

    Added router as the bridge for navigation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import './style.css'
+import './app.component.ts';
 import {OfferComponent} from "@dota/components/home/offer.component.ts";
 import {HeroSectionComponent} from "@dota/components/home/hero-section.component.ts";
 import {HeaderComponent} from "@dota/components/home/header.component.ts";
-import '@dota/pages/home.page'
 import {NotificationHolderComponent} from "@dota/components/utils/notification/notification-holder.component.ts";
 import {NotificationComponent} from "@dota/components/utils/notification/notification.component.ts";
+import {HomePage} from "@dota/pages/home.page.ts";
+import {DocPage} from "@dota/pages/doc.page.ts";
+import {AppComponent} from "@dota/app.component.ts";
+import {RoutesService} from "@dota/service/routes.service.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<home-page></home-page>
-`
+new RoutesService();
 
 
 declare global {
@@ -18,5 +20,8 @@ declare global {
         'app-hero': HeroSectionComponent,
         'notification-holder': NotificationHolderComponent
         'app-notification': NotificationComponent
+        'home-page': HomePage
+        'doc-page': DocPage,
+        'app-root': AppComponent
     }
 }

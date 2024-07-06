42fea7cf706fdcd839a4e9337c0c5d07e0ec9f55
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-07 01:06:50 +0530

    Create router.service.ts: Uses new Navigation API to handle navigate events

---
+++ src/service/routes.service.ts
@@ -0,0 +1,40 @@
+import {routes} from "@dota/routes.config.ts";
+import {AppComponent} from "@dota/app.component.ts";
+
+export class RoutesService {
+
+    constructor() {
+        this.init()
+    }
+
+    init() {
+        const navigation: Navigation = window.navigation;
+        navigation.addEventListener('navigate', (event: NavigateEvent) => {
+            if (!event.canIntercept || event.hashChange || event.downloadRequest !== null) {
+                return;
+            }
+
+            const url = new URL(event.destination.url);
+            let render = this.render;
+            event.intercept({
+                async handler() {
+                    console.log(url.pathname)
+                    render(url.pathname);
+                }
+            })
+        })
+
+
+    }
+
+    route(event: Event) {
+        const target: HTMLAnchorElement = event.target as HTMLAnchorElement;
+        const url: URL = new URL(target.href);
+        history.pushState({}, '', url.pathname)
+        this.render(url.pathname);
+    }
+
+    render(path: string) {
+        document.querySelector<AppComponent>('app-root')!.innerHTML = routes[path] || `<app-error path="${path}" message="Page Not Found" status=404 />`;
+    }
+}

4e942e472f539e6b449d3edf7e1983ec9c0c99e3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-08 22:10:32 +0530

    Update IconComponent: Fixed UI for dark and light mode, Use the new Decorator @AfterInit for initializing component after view init

--- src/components/utils/icons.component.ts
+++ src/components/utils/icons.component.ts
@@ -1,4 +1,4 @@
-import {BaseElement, BindEvent, Component, EventListener, HTML, Property} from "@ayu-sh-kr/dota-core/dist";
+import {BaseElement, BindEvent, Component, EventListener, HTML, Property, AfterInit} from "@ayu-sh-kr/dota-core/dist";
 
 
 @Component({
@@ -18,8 +18,8 @@ export class IconsComponent extends BaseElement {
     }
 
 
-    connectedCallback() {
-        super.connectedCallback();
+    @AfterInit()
+    afterViewInit() {
         this.getSvg().then();
     }
 
@@ -44,7 +44,7 @@ export class IconsComponent extends BaseElement {
 
     render(): string {
         return HTML`
-        <div id="svg" class="p-1.5 transition-all duration-300 dark:hover:bg-slate-200 hover:bg-slate-100 rounded-lg flex items-center justify-center">
+        <div id="svg" class="p-1.5 transition-all duration-300 dark:bg-slate-100 dark:hover:bg-slate-200 hover:bg-slate-100 rounded-lg flex items-center justify-center">
             
         </div> 
         `;

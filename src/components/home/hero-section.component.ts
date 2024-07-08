5bb3c157fac7ccd9552c9e8ae043ca3948c1fb8c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-08 22:09:32 +0530

    Update HeroSection: Fixed UI as per the dark and light mode

--- src/components/home/hero-section.component.ts
+++ src/components/home/hero-section.component.ts
@@ -25,23 +25,23 @@ export class HeroSectionComponent extends BaseElement {
         return HTML`
             <div class="flex flex-col items-center justify-center py-16 font-dm">
                 <h1 
-                    class="text-6xl sm:text-8xl font-extrabold text-gray-900 max-w-5xl text-wrap py-10 tracking-wide text-center">
+                    class="text-6xl sm:text-8xl font-extrabold text-gray-900 dark:text-gray-100 max-w-5xl text-wrap py-10 tracking-wide text-center">
                     Build. Customize. Distribute. <span class="bg-gradient-to-r from-purple-600 via-purple-600 to-purple-400 bg-clip-text text-transparent">Reuse.</span>
                 </h1>
                 
-                <div class="text-center flex flex-col gap-y-2">
-                    <p class="text-xl sm:text-2xl text-gray-800 tracking-wide">
+                <div class="text-center flex flex-col gap-y-2 text-gray-800 dark:text-gray-200">
+                    <p class="text-xl sm:text-2xl tracking-wide">
                         Dota is a library for building-reusable, customizable UI components libraries.
                     </p>
-                    <p class="text-xl sm:text-2xl text-gray-800 tracking-wide">
+                    <p class="text-xl sm:text-2xl tracking-wide">
                         Generate small, blazing fast Web Components that run everywhere.
                     </p>
                 </div>
                 
-                <div class="flex py-10 justify-center sm:justify-between items-center gap-y-5 gap-x-10 flex-wrap">
-                    <a href="/docs#get-started" class="text-2xl text-white bg-blue-500 rounded-3xl py-3 px-4">Get Started</a>
+                <div class="flex py-10 justify-center sm:justify-between items-center gap-y-5 gap-x-10 flex-wrap dark:text-gray-100 text-gray-900">
+                    <a href="/docs#get-started" class="text-2xl bg-blue-500 rounded-3xl py-3 px-4">Get Started</a>
                     <p  
-                        class="text-gray-800 text-xl px-4 py-3 border rounded-3xl flex items-center gap-x-2">
+                        class="text-xl px-4 py-3 border rounded-3xl flex items-center gap-x-2">
                         npm install @ayu-sh-kr/dota-core 
                         <span class="active:scale-95 cursor-pointer" id="copy"">
                             <app-icon name="material-symbols:content-copy-rounded"/>

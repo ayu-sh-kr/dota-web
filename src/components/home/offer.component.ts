20691f70d5205e334a0e49e36f808da27496bd07
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-09 01:18:24 +0530

    Update OfferComponent: Made it closable

--- src/components/home/offer.component.ts
+++ src/components/home/offer.component.ts
@@ -1,4 +1,4 @@
-import { BaseElement, Component, HTML } from "@ayu-sh-kr/dota-core/dist";
+import {BaseElement, BindEvent, Component, HTML} from "@ayu-sh-kr/dota-core/dist";
 
 @Component({
     selector: 'app-offer',
@@ -10,12 +10,20 @@ export class OfferComponent extends BaseElement {
         super();
     }
 
+    @BindEvent({event: 'click', id: '#close'})
+    handleClose(){
+        this.remove();
+    }
+
     render(): string {
         return HTML`
             <div 
-                class="font-helvetica flex items-center justify-center w-full bg-gradient-to-r from-purple-600 via-black to-purple-600 py-4"
+                class="relative font-dm flex items-center justify-center w-full bg-gradient-to-r from-purple-600 via-black to-purple-600 py-4"
             >
                 <div class="text-lg text-white gap-x-4 hover:text-opacity-90 cursor-pointer flex">Write <span class="text-purple-600"> Reusable </span> HTML</div>
+                <span id="close" class="absolute right-10 focus:scale-95 cursor-pointer">
+                        <app-icon name="ic:twotone-close"></app-icon>
+                </span>
             </div>
         `
     }

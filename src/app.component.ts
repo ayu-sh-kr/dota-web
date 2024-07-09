b0a8f5bf4fd155664e513752de011cac19cf67b2
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-09 16:03:17 +0530

    Code Refactor

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,8 @@
-import {BaseElement, BindEvent, Component, EventListener, HTML} from "@ayu-sh-kr/dota-core/dist";
+import {AfterInit, BaseElement, BindEvent, Component, EventListener, HTML} from "@ayu-sh-kr/dota-core/dist";
 import '@dota/pages/home.page.ts'
 import '@dota/pages/doc.page.ts'
 import '@dota/pages/error.page.ts'
+import {toggleDark} from "@dota/components/home/header.component.ts";
 
 
 @Component({
@@ -10,9 +11,8 @@ import '@dota/pages/error.page.ts'
 })
 export class AppComponent extends BaseElement {
 
-
-
-    handleUrlChange(){
+    @AfterInit()
+    handleUIMode(){
 
     }
 

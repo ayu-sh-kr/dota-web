f8ff37cb3d2931fe6c76a8b1282ec298b7b230a3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-09 23:56:41 +0530

    Code Refactoring and Cleanup

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,7 @@
-import {AfterInit, BaseElement, BindEvent, Component, EventListener, HTML} from "@ayu-sh-kr/dota-core/dist";
+import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core/dist";
 import '@dota/pages/home.page.ts'
 import '@dota/pages/doc.page.ts'
 import '@dota/pages/error.page.ts'
-import {toggleDark} from "@dota/components/home/header.component.ts";
 
 
 @Component({
@@ -10,16 +9,6 @@ import {toggleDark} from "@dota/components/home/header.component.ts";
     shadow: false
 })
 export class AppComponent extends BaseElement {
-
-    @AfterInit()
-    handleUIMode(){
-
-    }
-
-    @BindEvent({event: 'route', id: 'a'})
-    handleRoutes(event: Event){
-        console.log(this)
-    }
 
     render(): string {
         return HTML`<home-page></home-page>`

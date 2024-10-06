8238bf80195eb4600ca43dd7bd7f3cdb47db0ebe
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-06 14:44:16 +0530

    Update main.ts
    update imports and register the components using bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import './style.css'
-import './app.component.ts';
-import {OfferComponent} from "@dota/components/home/offer.component.ts";
-import {HeroSectionComponent} from "@dota/components/home/hero-section.component.ts";
-import {HeaderComponent} from "@dota/components/home/header.component.ts";
 import {NotificationHolderComponent} from "@dota/components/utils/notification/notification-holder.component.ts";
 import {NotificationComponent} from "@dota/components/utils/notification/notification.component.ts";
-import {HomePage} from "@dota/pages/home.page.ts";
-import {DocPage} from "@dota/pages/doc.page.ts";
 import {AppComponent} from "@dota/app.component.ts";
 import {RoutesService} from "@dota/service/routes.service.ts";
+import {bootstrap} from "@ayu-sh-kr/dota-core";
+import {DocPage, ErrorPage, HomePage} from "@dota/pages";
+import {FeatureComponent, HeaderComponent, HeroSectionComponent, OfferComponent} from "@dota/components/home";
+import {IconsComponent} from "@ayu-sh-kr/dota-ui/dist";
+
+
+bootstrap([
+    OfferComponent,
+    HeroSectionComponent,
+    HeaderComponent,
+    NotificationComponent,
+    NotificationHolderComponent,
+    HomePage,
+    DocPage,
+    ErrorPage,
+    FeatureComponent,
+    IconsComponent,
+    AppComponent
+])
 
 new RoutesService();
 

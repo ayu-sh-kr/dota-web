4c87987bf518709918ae92b58e0220d5449ff1ed
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-06 14:44:24 +0530

    Update doc.page.ts
    added constructor for initialization as abstract class BaseElement is unable to

--- src/pages/doc.page.ts
+++ src/pages/doc.page.ts
@@ -7,6 +7,10 @@ import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core/dist";
 })
 export class DocPage extends BaseElement {
 
+    constructor() {
+        super();
+    }
+
     render(): string {
         return HTML`
         <app-header/>

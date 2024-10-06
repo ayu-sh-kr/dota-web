688fef835473fbd95874e7861d8077c198b2265c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-06 14:45:17 +0530

    Create index.ts
    directory level index file for better export mechanism

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import {DocPage} from "@dota/pages/doc.page.ts";
+import {ErrorPage} from "@dota/pages/error.page.ts";
+import {HomePage} from "@dota/pages/home.page.ts";
+
+export {DocPage, ErrorPage, HomePage}

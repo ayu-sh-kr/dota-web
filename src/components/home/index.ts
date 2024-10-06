62b1ebbc21bbafcea360818719f1c4a6d3c92e6f
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-06 14:41:36 +0530

    Create index.ts
    create sub directory level index file to export all the files for better readability

---
+++ src/components/home/index.ts
@@ -0,0 +1,6 @@
+import {HeroSectionComponent} from "@dota/components/home/hero-section.component.ts";
+import {HeaderComponent} from "@dota/components/home/header.component.ts";
+import {OfferComponent} from "@dota/components/home/offer.component.ts";
+import {FeatureComponent} from "@dota/components/home/feature.component.ts";
+
+export {HeaderComponent, HeroSectionComponent, OfferComponent, FeatureComponent}

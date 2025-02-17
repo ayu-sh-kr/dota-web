fabb0119c2fa8c2a5c5e7f4487f9e62e481a10dc
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-17 18:47:02 +0530

    exported component and renamed them

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,8 +1,15 @@
-import { HeroSectionComponent } from "@dota/components/home/hero-section.component.ts";
-import { HeaderComponent } from "@dota/components/home/header.component.ts";
-import { OfferComponent } from "@dota/components/home/offer.component.ts";
-import { FeatureComponent } from "@dota/components/home/feature.component.ts";
-import { CodeSection } from "@dota/components/home/code-section.component.ts";
-import { DeviceSectionComponent } from "@dota/components/home/device-section..component.ts";
+import {HeroSectionComponent} from "@dota/components/home/hero-section.component.ts";
+import {HeaderComponent} from "@dota/components/home/header.component.ts";
+import {OfferComponent} from "@dota/components/home/offer.component.ts";
+import {FeatureComponent} from "@dota/components/home/feature.component.ts";
+import {CodeSectionComponent} from "@dota/components/home/code-section.component.ts";
+import {DeviceSectionComponent} from "@dota/components/home/device-section..component.ts";
 
-export { HeaderComponent, HeroSectionComponent, OfferComponent, FeatureComponent, CodeSection, DeviceSectionComponent };
+export {
+    HeaderComponent,
+    HeroSectionComponent,
+    OfferComponent,
+    FeatureComponent,
+    CodeSectionComponent,
+    DeviceSectionComponent
+};

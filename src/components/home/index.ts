81d2778c876cd49bd44942c36c6d3f3632ab579b
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-17 18:53:28 +0530

    refactoring changes

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -3,7 +3,7 @@ import {HeaderComponent} from "@dota/components/home/header.component.ts";
 import {OfferComponent} from "@dota/components/home/offer.component.ts";
 import {FeatureComponent} from "@dota/components/home/feature.component.ts";
 import {CodeSectionComponent} from "@dota/components/home/code-section.component.ts";
-import {DeviceSectionComponent} from "@dota/components/home/device-section..component.ts";
+import {DeviceSectionComponent} from "@dota/components/home/device-section.component.ts";
 
 export {
     HeaderComponent,

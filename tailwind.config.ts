635db656677ab5abe9200ec873eb088cddf9bf5c
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-18 16:30:21 +0530

    added animation for `GetStarted` button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,22 @@ export default<Partial<Config>> {
         'system': ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"'],
         'roboto': ['Roboto', 'sans-serif'],
       },
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(0.6deg)',
+          },
+          '75%': {
+            transform: 'rotate(-0.6deg)',
+          },
+        },
+      },
     },
   },
   plugins: [],

84f5f9a89071f37e74b0bc758c8cb7748606dcef
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-18 16:27:22 +0530

    moved `HeaderComponent` to util

--- src/components/utils/header.component.ts
+++ src/components/utils/header.component.ts
@@ -1,4 +1,4 @@
-import {BaseElement, BindEvent, Component, HTML} from "@ayu-sh-kr/dota-core";
+import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core";
 
 @Component({
     selector: 'app-header',
@@ -32,37 +32,22 @@ export class HeaderComponent extends BaseElement {
         super();
     }
 
-    @BindEvent({event: 'click', id: '#dark-button'})
-    handleDark() {
-        const iconContainer = this.querySelector('#dark-button');
-
-       if(iconContainer) {
-           if(toggleDark()) {
-               iconContainer.innerHTML = `<dota-icon  name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`
-           }else {
-                iconContainer.innerHTML = `<dota-icon  name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`
-           }
-       }
-
-    }
-
 
     render(): string {
         return HTML`
             <header class="flex z-50 justify-between items-center px-2 py-3 font-dm sticky top-0 left-0 shadow-md bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
-                <div class="font-extrabold text-2xl w-1/6 px-5"><a href="/">Logo</a></div>
+                <div class="font-extrabold text-2xl w-1/6 px-5"><a href="/public">Logo</a></div>
                 <div class="md:flex justify-center items-center hidden w-4/6">
                     <ul class="flex justify-between items-center gap-x-5 w-1/2">
                         ${this.items.map((item) => {
-                            return `<li class="text-sm font-semibold hover:text-purple-600"><a href="${item.url}">${item.name}</a></li>`
-                        }).join('')}
+            return `<li class="text-sm font-semibold hover:text-purple-600"><a href="${item.url}">${item.name}</a></li>`
+        }).join('')}
                     </ul>
                 </div>
                 <div class="w-1/6">
-                    <div class="flex items-center px-5 justify-end">
-                        <span id="dark-button" class="active:scale-95 cursor-pointer">
-                            <dota-icon  name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />
-                        </span>
+                    <div class="flex items-center px-5 gap-x-3 justify-end">
+                        <github-button></github-button>
+                        <dark-mode-button></dark-mode-button>
                     </div>
                 </div>
             </header>

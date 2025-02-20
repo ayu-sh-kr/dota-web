8d381794782ffefe4f39a625556ab2fe3c9d6e1d
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-20 17:01:06 +0530

    fix layout

--- src/components/home/code-section.component.ts
+++ src/components/home/code-section.component.ts
@@ -12,18 +12,19 @@ export class CodeSectionComponent extends BaseElement {
     render(): string {
         // language=HTML
         return HTML`
-            <section class="font-dm">
-                <div class="grid md:grid-cols-2 gap-x-20 gap-y-10 py-20 px-10 sm:px-20 place-items-center justify-items-center">
+            <section class="font-dm mx-auto max-w-7xl px-3 py-2 mt-10">
+                <div class="grid md:grid-cols-2 gap-x-20 gap-y-10 place-items-center justify-items-center">
                     <div class="space-y-5">
                       <h4 class="text-gray-700 dark:text-gray-300 font-semibold">Component Libraries</h4>
                       <h2 class="ld:text-5xl md:text-4xl text-3xl font-extrabold tracking-wide text-gray-950 dark:text-gray-100">Web Component Tool.</h2>
                       <p class="text-gray-600 dark:text-gray-400 font-light lg:text-2xl md:text-xl text-lg">
                           Ensure consistent UX and brand experiences at scale with components that run on any platform or device. 
-                        Build custom UIs that work seamlessly across teams and projects.
+                          Build custom UIs that work seamlessly across teams and projects.
                       </p>
                     </div>
                     <img src="/images/code-snippet-1.png" alt="dot-ui-bg"
-                         class="rounded-lg mix-blend-darken min-w-[360px] max-w-[80%]"/>
+                         class="rounded-lg mix-blend-darken min-w-[360px] max-w-[80%]"
+                    />
                 </div>
             </section>
         `;
--- src/components/home/our-tools.component.ts
+++ src/components/home/our-tools.component.ts
@@ -5,60 +5,55 @@ import { BaseElement, Component } from "@ayu-sh-kr/dota-core";
   shadow: false,
 })
 export class OurToolsComponent extends BaseElement {
+
   constructor() {
     super();
   }
 
   render(): string {
+
+    // language=HTML
     return `
-            <h1 class="text-5xl sm:text-7xl font-extrabold text-gray-900 dark:text-gray-100 text-wrap py-10 leading-[3.25rem] tracking-wide text-center">Our Tools</h1>
-
-              <div class="flex flex-wrap py-20 justify-items-center  place-items-center justify-between px-10 mt-20 gap-20 ">
-
-            <article class="card-wrapper h-[180px]  mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
+      <section class="mx-auto max-w-7xl px-3 py-2 mt-10">
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-7">
+          <article
+              class="card-wrapper h-[180px]  mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
             <div class="card-content pt-6 flex flex-col gap-y-4 items-center">
-             <p class="text-lg font-semibold pt-4">
-             Dota Core
+              <p class="text-lg font-semibold pt-4">
+                Dota Core
               </p>
-            <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
-            Dota Core is a library for building-reusable, customizable UI components libraries.
-             </p>
-             </div>
-            </article>
-           
-             <article class="card-wrapper h-[180px]  mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
-            <div class="card-content delay-200 pt-6 flex flex-col gap-y-4 items-center">
-             <p class="text-lg font-semibold pt-4">
-            Dota Web
+              <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
+                Dota Core is a library for building-reusable, customizable UI components libraries.
               </p>
-            <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
-            Dota Web is a library for building-reusable, customizable UI components libraries.
-             </p>
-             </div>
-            </article>
-         
-              <article class="card-wrapper h-[180px] mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
-            <div class="card-content delay-500 pt-6 flex flex-col gap-y-4 items-center">
-             <p class="text-lg font-semibold pt-4">
-             Dota Core
-              </p>
-            <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
-             Dota Core is a library for building-reusable, customizable UI components libraries
-             </p>
-             </div>
-            </article>
-          
-           <article class="card-wrapper  h-[180px] mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
-            <div class="card-content  pt-6 flex flex-col gap-y-4 items-center">
-             <p class="text-lg font-semibold pt-4">
-               Web
-              </p>
-            <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
-            The Dota Web is a library for building-reusable, customizable UI components libraries.
-             </p>
-             </div>
-            </article>
             </div>
-        `;
+          </article>
+
+          <article
+              class="card-wrapper h-[180px]  mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
+            <div class="card-content delay-200 pt-6 flex flex-col gap-y-4 items-center">
+              <p class="text-lg font-semibold pt-4">
+                Dota Rest
+              </p>
+              <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
+                Dota Rest is wrapper around fetch API, provides fluent API based methods for performing Rest based requests.
+              </p>
+            </div>
+          </article>
+
+          <article
+              class="card-wrapper h-[180px] mx-auto w-[340px] p-4 bg-white  rounded-lg shadow-md dark:text-gray-100 hover:scale-110 ease-in-out duration-300 hover:shadow-2xl">
+            <div class="card-content delay-500 pt-6 flex flex-col gap-y-4 items-center">
+              <p class="text-lg font-semibold pt-4">
+                Dota UI
+              </p>
+              <p class="text-gray-700 max-w-[85%] dark:text-gray-300">
+                Dota UI is a UI library which provides collection of web component ready to be served for your next project.
+              </p>
+            </div>
+          </article>
+        </div>
+      </section>
+        
+    `;
   }
 }

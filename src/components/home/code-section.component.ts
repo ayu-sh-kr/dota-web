ed3cae385f130b142407dcf0ef7b822c6f387604
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-02-20 16:33:56 +0530

    fix code-section component and added content

--- src/components/home/code-section.component.ts
+++ src/components/home/code-section.component.ts
@@ -1,32 +1,31 @@
-import { BaseElement, Component, HTML } from "@ayu-sh-kr/dota-core";
+import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core";
 
 @Component({
-  selector: "code-section",
-  shadow: false,
+    selector: "code-section",
+    shadow: false,
 })
 export class CodeSectionComponent extends BaseElement {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  render(): string {
-    return HTML`
-          <section>
-            <h1 class="text-4xl sm:text-6xl font-extrabold text-gray-900 dark:text-gray-100 text-center text-wrap leading-[3.25rem] tracking-wide py-20 ">Clean And Efficient Code Architecture</h1>
-        
-
-            <div class="grid md:grid-cols-2 gap-x-20 gap-y-10 py-20 px-10 sm:px-20 place-items-center justify-items-center">
-            <div class="space-y-20">
-            <h1 class="text-4xl font-semibold">Reactive support</h1>
-             <p class="text-lg">
-                With reactivity dom gets update each time a property marked as @Property gets its value changed.
-                </p>         
-             </div>
-             <img src="/images/code-snippet-1.png" alt="dot-ui-bg" class="rounded-lg mix-blend-darken min-w-[360px] max-w-[80%]" />
-            </div>
-            
-          
-          </section>
+    render(): string {
+        // language=HTML
+        return HTML`
+            <section class="font-dm">
+                <div class="grid md:grid-cols-2 gap-x-20 gap-y-10 py-20 px-10 sm:px-20 place-items-center justify-items-center">
+                    <div class="space-y-5">
+                      <h4 class="text-gray-700 dark:text-gray-300 font-semibold">Component Libraries</h4>
+                      <h2 class="ld:text-5xl md:text-4xl text-3xl font-extrabold tracking-wide text-gray-950 dark:text-gray-100">Web Component Tool.</h2>
+                      <p class="text-gray-600 dark:text-gray-400 font-light lg:text-2xl md:text-xl text-lg">
+                          Ensure consistent UX and brand experiences at scale with components that run on any platform or device. 
+                        Build custom UIs that work seamlessly across teams and projects.
+                      </p>
+                    </div>
+                    <img src="/images/code-snippet-1.png" alt="dot-ui-bg"
+                         class="rounded-lg mix-blend-darken min-w-[360px] max-w-[80%]"/>
+                </div>
+            </section>
         `;
-  }
+    }
 }

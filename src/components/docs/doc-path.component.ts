28d5fa44797551b36b4b288cc2966d3bf3984670
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-14 01:24:57 +0530

    feat: update doc-path component to utilize routing service and enhance path rendering

--- src/components/docs/doc-path.component.ts
+++ src/components/docs/doc-path.component.ts
@@ -1,4 +1,6 @@
-import {BaseElement, Component, Emitter, EventEmitter, HostListener, Property, String} from "@ayu-sh-kr/dota-core";
+import {BaseElement, Component, HostListener, Property, String} from "@ayu-sh-kr/dota-core";
+import {RoutesService} from "@dota/service/routes.service.ts";
+import {RouteUtils} from "@dota/utils/RouteUtils.ts";
 
 @Component({
   selector: 'doc-path',
@@ -12,23 +14,20 @@ export class DocPathComponent extends BaseElement {
   })
   filePath!: string;
 
-  @Emitter()
-  filePathChange!: EventEmitter<string>
-
   constructor() {
     super();
   }
 
   @HostListener({event: 'click'})
   clickListener() {
-    console.log('Clicked path')
-    this.filePathChange.emit(this.filePath);
+    RoutesService.route(`/docs/${this.filePath}`);
   }
 
   render(): string {
+    const includes = RouteUtils.getCurrentEntry().includes(this.filePath);
     // language=html
     return `
-    <div class="cursor-pointer text-sm text-gray-600 dark:text-gray-300 py-1 px-2 
+    <div class="cursor-pointer text-sm text-gray-600 dark:text-gray-300 py-1 px-2 ${includes ? 'text-purple-600 dark:text-purple-500' : ''}
         hover:text-purple-600 dark:hover:text-purple-500 transition-all hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg"
     >
       ${this.filePath.replace('.md', '').replace('-', ' ')}

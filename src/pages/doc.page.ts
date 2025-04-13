024310ecb7498e6bcc5081db0a1ed26556ab7c83
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-13 22:59:26 +0530

    feat: enhance documentation sidebar with dynamic configuration

--- src/pages/doc.page.ts
+++ src/pages/doc.page.ts
@@ -1,5 +1,6 @@
 import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core/dist";
 import {Property, String, WindowListener} from "@ayu-sh-kr/dota-core";
+import {docConfigs} from "@dota/doc.config.ts";
 
 
 @Component({
@@ -31,11 +32,16 @@ export class DocPage extends BaseElement {
     return HTML`
       <app-header></app-header>
       <section class="flex gap-4 relative">
-        <aside class="sticky top-14 h-[calc(100vh-50px)] hidden md:block w-72 bg-white p-4  dark:bg-gray-900 overflow-y-auto text-sm space-y-4 border-r custom-scrollbar">
-          <doc-path file-path="Getting-Started.md"></doc-path>
-          <doc-path file-path="Component-Registration.md"></doc-path>
-          <doc-path file-path="Component-Definition.md"></doc-path>
-          <doc-path file-path="Property-Binding.md"></doc-path>
+        <aside class="sticky top-14 h-[calc(100vh-50px)] hidden md:block w-72 bg-white p-4 dark:bg-gray-900 overflow-y-auto text-sm space-y-4 border-r custom-scrollbar">
+          ${docConfigs.map(config => {
+            // language=html
+            return `
+            <div class="border-b-2 py-2">
+                <h2>${config.category}</h2>
+                ${config.paths.map(path => `<doc-path file-path="${path}"></doc-path>`).join(" ")}
+            </div>
+            `
+          }).join(" ")}
         </aside>
         <main class="flex-1 p-4">
           <doc-content file-path="${this.contentPath}"></doc-content>
@@ -45,5 +51,4 @@ export class DocPage extends BaseElement {
       <notification-holder id="dota-notification" position="bottom-right"></notification-holder>
     `;
   }
-
 }

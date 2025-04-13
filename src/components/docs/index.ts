a42f051e8eb77eb6a3d0783de051b63299561c69
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-13 18:51:08 +0530

    add: export BlogPage, ReactiveSectionComponent, and HostListenerSectionComponent in index.ts

---
+++ src/components/docs/index.ts
@@ -0,0 +1,4 @@
+import {DocContentComponent} from "@dota/components/docs/doc-content.component.ts";
+import {DocPathComponent} from "@dota/components/docs/doc-path.component.ts";
+
+export {DocContentComponent, DocPathComponent};

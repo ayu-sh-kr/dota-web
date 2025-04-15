b33f542499b21e5cdfb9a9e1112d8a9b90c367cc
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-15 21:17:15 +0530

    Squashed commit of the following:
    commit f533720bba0bf72e23fcc111e55cdf9f8156cf99
    Author: ayu-sh-kr <akjaiswal2003@gmail.com>
    Date:   Tue Apr 15 21:16:33 2025 +0530
    
        feat: add new components to main.ts for improved application structure
    
    commit cab7b9a691698ea6e65c3bd5b5c13fb1d497053b
    Author: ayu-sh-kr <akjaiswal2003@gmail.com>
    Date:   Tue Apr 15 21:16:25 2025 +0530
    
        fix: enhance error handling in DocLoaderService by routing to error page on response failure
    
    commit 0cf9200644985add5290d407e12198ff4eafe05b
    Author: ayu-sh-kr <akjaiswal2003@gmail.com>
    Date:   Tue Apr 15 21:16:17 2025 +0530
    
        fix: remove hardcoded content path and sanitize file path in doc-content.component.ts
    
    commit 2ff33245d017317e698aa4f9fb0fe1318d9663d5
    Author: ayu-sh-kr <akjaiswal2003@gmail.com>
    Date:   Tue Apr 15 21:16:06 2025 +0530
    
        fix: remove hardcoded content path in doc.page.ts

--- src/service/doc-loader.service.ts
+++ src/service/doc-loader.service.ts
@@ -1,22 +1,29 @@
 import {RestClient} from "@ayu-sh-kr/dota-rest";
+import {RoutesService} from "@dota/service/routes.service.ts";
 
 
 export class DocLoaderService {
 
-    restClient: RestClient;
+  restClient: RestClient;
 
-    constructor() {
-        this.restClient = RestClient.builder()
-            .timeout(10000)
-            .build();
-    }
+  constructor() {
+    this.restClient = RestClient.builder()
+      .timeout(10000)
+      .handler((response) => {
+        console.log(response)
+        if(!response.ok) {
+          RoutesService.route('/error')
+        }
+      })
+      .build();
+  }
 
-    async loadDoc(path: string): Promise<string> {
-        const response = await this.restClient.get()
-            .uri(`/documents/${path}`)
-            .retrieve()
-            .toResponse();
+  async loadDoc(path: string): Promise<string> {
+    const response = await this.restClient.get()
+      .uri(`/documents/${path}`)
+      .retrieve()
+      .toResponse();
 
-        return response.text();
-    }
+    return response.text();
+  }
 }

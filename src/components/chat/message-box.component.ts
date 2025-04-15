89046e97734459001e5051f701f793dbf2a10c9a
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-16 00:23:46 +0530

    feat: add MessageBoxComponent for displaying messages in a styled section

---
+++ src/components/chat/message-box.component.ts
@@ -0,0 +1,28 @@
+import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
+
+@Component({
+  selector:"message-box",
+  shadow:false
+})
+export class MessageBoxComponent extends  BaseElement{
+
+  content:string
+
+  constructor(){
+    super()
+    this.content=this.innerHTML
+  }
+
+
+
+
+
+  render(){
+    // language=html
+    return `
+        <section class="h-[50vh] overflow-y-auto custom-scrollbar p-4 bg-slate-100 dark:bg-slate-900 rounded-lg flex flex-col gap-4 ">
+         ${this.content}
+        </section>
+    `
+  }
+}

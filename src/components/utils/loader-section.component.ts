30d8acf7260cdd7785db9db41c14c72fd634516e
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-16 15:33:36 +0530

    feat: add LoaderSectionComponent for improved loading state management

---
+++ src/components/utils/loader-section.component.ts
@@ -0,0 +1,29 @@
+import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
+import {LoaderComponent} from "@dota/components/utils/loader.component.ts";
+import {Boolean, Property} from "@ayu-sh-kr/dota-core";
+
+@Component({
+  selector:"loader-section",
+  shadow:false
+})
+export class LoaderSectionComponent extends BaseElement{
+
+  @Property({
+    name: 'is-loader',
+    type: Boolean
+  })
+  isLoading: boolean = false;
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    //language=html
+    return `
+      <section class="${this.isLoading ? 'fixed' : 'hidden'} z-50 top-0 left-0 h-screen bg-black/50 w-full flex items-center justify-center">
+        <app-loader></app-loader>
+      </section>
+    `;
+  }
+}

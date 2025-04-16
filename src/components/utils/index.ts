b13f1c2f5628d52738d3f5132656cb258551afee
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-16 11:52:10 +0530

    feat: add AiButtonComponent export to index for improved accessibility

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -4,9 +4,11 @@ import {NotificationHolderComponent} from "@dota/components/utils/notification/n
 import {GithubButtonComponent} from "@dota/components/utils/buttons/github-button.component.ts";
 import {DarkModeButtonComponent} from "@dota/components/utils/buttons/dark-mode-button.component.ts";
 import {FooterComponent} from "@dota/components/utils/footer.component.ts";
+import {AiButtonComponent} from "@dota/components/utils/buttons/ai-button.component.ts";
 
 export {
   HeaderComponent,
+  AiButtonComponent,
   GithubButtonComponent,
   DarkModeButtonComponent,
   NotificationComponent,

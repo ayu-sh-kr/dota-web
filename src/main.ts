48816ee7eb2c457f0faf672afe640817ac06a238
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-16 11:52:16 +0530

    feat: add AiButtonComponent import to main for improved accessibility

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import {
 } from "@dota/components/home";
 import {IconsComponent} from "@ayu-sh-kr/dota-ui/dist";
 import {
+  AiButtonComponent,
   DarkModeButtonComponent, FooterComponent,
   GithubButtonComponent,
   NotificationComponent,
@@ -35,6 +36,7 @@ bootstrap([
   DarkModeButtonComponent,
   GithubButtonComponent,
   IconsComponent,
+  AiButtonComponent,
 
   // home-page components
   OfferComponent,

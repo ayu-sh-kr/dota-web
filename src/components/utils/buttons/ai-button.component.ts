4dc93353eca7cec52cbf216e35db123ffcf15cbe
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-16 11:51:41 +0530

    feat: add AiButtonComponent for routing to chat page on click

---
+++ src/components/utils/buttons/ai-button.component.ts
@@ -0,0 +1,32 @@
+import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
+import {HostListener} from "@ayu-sh-kr/dota-core";
+import {RoutesService} from "@dota/service/routes.service.ts";
+
+
+@Component({
+  selector: 'ai-button',
+  shadow: false,
+})
+export class AiButtonComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  @HostListener({event: 'click'})
+  hostClickListener() {
+    RoutesService.route('/chat');
+  }
+
+  render(): string {
+    // language=html
+    return `
+      <span id="github-button" class="active:scale-95 cursor-pointer">
+        <a target="_blank">
+            <dota-icon name="healthicons:artificial-intelligence" color="pink" variant="ghost" size="lg"/>
+        </a>
+      </span>
+    `;
+  }
+
+}

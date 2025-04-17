43a8d1888db1f52cac9346201ae77e5e8b1f41d6
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-17 23:20:50 +0530

    fix: update routing method in ai-button.component.ts to use DomNavigationRouter

--- src/components/utils/buttons/ai-button.component.ts
+++ src/components/utils/buttons/ai-button.component.ts
@@ -1,6 +1,7 @@
 import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
 import {HostListener} from "@ayu-sh-kr/dota-core";
 import {NavigationRouterService} from "@dota/service/routes.service.ts";
+import {DomNavigationRouter} from "@ayu-sh-kr/dota-router";
 
 
 @Component({
@@ -15,7 +16,7 @@ export class AiButtonComponent extends BaseElement {
 
   @HostListener({event: 'click'})
   hostClickListener() {
-    NavigationRouterService.route('/chat');
+    DomNavigationRouter.route('/chat');
   }
 
   render(): string {

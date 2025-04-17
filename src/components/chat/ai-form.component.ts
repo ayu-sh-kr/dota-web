bf1f3452fffa312e334b7f8f4b349cba07f11b0e
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-17 09:38:50 +0530

    fix: update input field attributes for better accessibility

--- src/components/chat/ai-form.component.ts
+++ src/components/chat/ai-form.component.ts
@@ -56,7 +56,7 @@ export class AiFormComponent extends BaseElement {
   render(): string {
     // language=html
     return `
-      <div class="relative group flex-1 ">
+      <div class="relative group flex-1 w-full">
         ${this.isLoading ? `<div class="absolute border -inset-1 bg-gradient-to-r dark:from-pink-400 dark:to-purple-400 from-pink-600 to-purple-600 
                     rounded-2xl blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"
                 >
@@ -67,7 +67,7 @@ export class AiFormComponent extends BaseElement {
             <img src="https://icon-library.com/images/bot-icon/bot-icon-29.jpg" alt="dota-bot"
                  class="size-7"/>
           </div>
-          <input name="user-input" type="text"  class="disabled:pointer-events-none disabled:opacity-70 w-full dark:text-gray-400  outline-none bg-transparent focus:border-purple-600 border-2  p-4 rounded-lg border-transparent "
+          <input name="user-input" type="text" autocomplete="off"   class="disabled:pointer-events-none disabled:opacity-70 w-full dark:text-gray-400  outline-none bg-transparent focus:border-purple-600 border-2  p-4 rounded-lg border-transparent "
                  placeholder="Hello! How can I assist you today?"/>
         </form>
       </div>

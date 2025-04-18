36b1353be49ad70fda9ce0a48d9d7b57fff75c22
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-19 00:12:33 +0530

    feat: enhance dark mode button to set initial theme based on browser settings and persist theme choice

--- src/components/utils/buttons/dark-mode-button.component.ts
+++ src/components/utils/buttons/dark-mode-button.component.ts
@@ -1,38 +1,57 @@
 import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
-import {BindEvent} from "@ayu-sh-kr/dota-core";
-import {toggleDark} from "@dota/components/utils/header.component.ts";
+import {AfterInit, BindEvent} from "@ayu-sh-kr/dota-core";
+
+import {GeneralUtils, toggleDark} from "@dota/utils/GeneralUtils.ts";
+import {LocalStorageService} from "@dota/service/local-storage.service.ts";
 
 
 @Component({
-    selector: 'dark-mode-button',
-    shadow: false
+  selector: 'dark-mode-button',
+  shadow: false
 })
-export class DarkModeButtonComponent extends BaseElement{
+export class DarkModeButtonComponent extends BaseElement {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
+  }
+
+  @AfterInit()
+  afterViewInit() {
+    const browserTheme = GeneralUtils.getBrowserTheme();
+    switch (browserTheme) {
+      case 'dark':
+        this.querySelector('#dark-button')!.innerHTML = `<dota-icon name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`;
+        break;
+      case 'light':
+        this.querySelector('#dark-button')!.innerHTML = `<dota-icon name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`;
+        break;
+      default:
+        break;
     }
+  }
 
-    @BindEvent({event: 'click', id: '#dark-button'})
-    handleDark() {
-        const iconContainer = this.querySelector('#dark-button');
+  @BindEvent({event: 'click', id: '#dark-button'})
+  handleDark() {
+    const iconContainer = this.querySelector('#dark-button');
 
-        if(iconContainer) {
-            if(toggleDark()) {
-                iconContainer.innerHTML = `<dota-icon  name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`
-            }else {
-                iconContainer.innerHTML = `<dota-icon  name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`
-            }
-        }
+    if (iconContainer) {
+      if (toggleDark()) {
+        iconContainer.innerHTML = `<dota-icon  name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`
+        LocalStorageService.add('theme', 'dark');
+      } else {
+        iconContainer.innerHTML = `<dota-icon  name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`
+        LocalStorageService.add('theme', 'light');
+      }
     }
+  }
 
-    render(): string {
-        // language=HTML
-        return `
-            <span id="dark-button" class="active:scale-95 cursor-pointer">
-                <dota-icon name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md"/>
-            </span>
-        `;
-    }
+  render(): string {
+    // language=html
+    return `
+      <span id="dark-button" class="active:scale-95 cursor-pointer">
+        <dota-icon name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md"/>
+      </span>
+    `;
+  }
 
 }

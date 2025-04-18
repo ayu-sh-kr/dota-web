70d507052ead77ccc4c1bcdd85bf9ca7d9b8e68f
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-19 00:12:51 +0530

    fix: improve text alignment and clarity in error page message

--- src/pages/error.page.ts
+++ src/pages/error.page.ts
@@ -34,8 +34,10 @@ export class ErrorPage extends BaseElement {
         <dota-icon class="scale-150" name="emojione-monotone:confounded-face" color="purple" variant="ghost" size="2xl"></dota-icon>
         <h1 class="text-5xl sm:text-7xl py-4 text-center font-semibold text-purple-600">404</h1>
         <p class="text-center text-2xl sm:text-3xl text-gray-500 dark:text-gray-100 font-medium">Page Not Found</p>
-        <p class="max-sm:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-200">The document you're looking for doesn't exist or has been
-          moved to another location.</p>
+        <p class="max-sm:text-sm lg:text-lg font-medium text-gray-900 dark:text-gray-200 text-center">
+          The document you're looking for doesn't exist or has been
+          moved to another location
+          .</p>
         <div class="items-center flex gap-2">
           <a href="/" class="bg-purple-600 text-white flex items-center gap-1 px-5 py-3 rounded-lg">
             <dota-icon name="mdi:home" variant="ghost" size="md"></dota-icon>

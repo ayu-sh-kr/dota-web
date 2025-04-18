c974b708c23b154da42836f5160a729c7c9553f9
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-18 22:16:44 +0530

    feat: update device section to use DevicePreview components for improved display

--- src/components/home/device-section.component.ts
+++ src/components/home/device-section.component.ts
@@ -1,4 +1,4 @@
-import { BaseElement, Component, HTML } from "@ayu-sh-kr/dota-core";
+import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core";
 
 
 @Component({
@@ -12,16 +12,19 @@ export class DeviceSectionComponent extends BaseElement {
   }
 
   render(): string {
+    // language=html
     return HTML`
-            <section class="font-dm mx-auto max-w-7xl px-6  py-20">
-            <h1 class="text-3xl sm:text-4xl font-extrabold font-adaptive text-center mb-20">A single component library for all of your frameworks.</h1>
-                <div class="grid grid-cols-2 md:grid-cols-4 place-items-center gap-10 ">
-                  <img src="/images/react-img.png" alt="dota-react" class="framework-logo hover:drop-shadow-[0px_10px_15px_rgba(34,211,238,0.8)]"/>
-                  <img src="/images/angular-img.png" alt="dota-angular" class="framework-logo hover:drop-shadow-[0px_10px_15px_rgba(220,38,38,0.8)]"/>
-                  <img src="/images/vue-img.png" alt="dota-vue" class="framework-logo  hover:drop-shadow-[0px_10px_15px_rgba(22,163,74,0.8)] "/>
-                  <img src="/images/solid-logo.png" alt="dota-solid" class="framework-logo hover:drop-shadow-[0px_10px_15px_rgb(147,51,234,0.8)]"/>
-                </div>
-            </section>
+      <section class="font-dm mx-auto max-w-7xl px-6  py-20">
+        <h1 class="text-3xl sm:text-4xl font-extrabold font-adaptive text-center mb-20">A single component library for
+          all of your frameworks.
+        </h1>
+        <div class="grid grid-cols-2 md:grid-cols-4 place-items-center gap-10 ">
+          <device-preview label="React" image="/images/react-img.png" color="blue"></device-preview>
+          <device-preview label="Angular" image="/images/angular-img.png" color="pink"></device-preview>
+          <device-preview label="Vue" image="/images/vue-img.png" color="emerald"></device-preview>
+          <device-preview label="Solid" image="/images/solid-logo.png" color="purple"></device-preview>
+        </div>
+      </section>
     `;
   }
 }

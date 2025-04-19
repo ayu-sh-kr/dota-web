2eb16143e041897dceb7eca73fa6b806f254a36f
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-19 18:24:49 +0530

    Feat/header: enhance header layout and styles with improved navigation and responsiveness

--- src/components/utils/header.component.ts
+++ src/components/utils/header.component.ts
@@ -33,26 +33,29 @@ export class HeaderComponent extends BaseElement {
 
   render(): string {
     return HTML`
-            <header class="flex z-50 justify-between items-center px-2 py-3 font-dm sticky top-0 left-0 
-                    shadow-md bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100 max-w-7xl mx-auto"
-            >
-                <div class="font-extrabold text-2xl w-1/6 px-5"><a href="/">Dota</a></div>
-                <div class="md:flex justify-center items-center hidden w-4/6">
-                    <ul class="flex justify-between items-center gap-x-5 w-1/2">
-                        ${this.items
-                          .map((item) => {
-                            return `<li class="text-sm font-semibold hover:text-purple-600"><a href="${item.url}">${item.name}</a></li>`;
-                          })
-                          .join("")}
-                    </ul>
-                </div>
-                <div class="w-1/6">
-                    <div class="flex items-center px-5 gap-x-3 justify-end">
-                        <github-button></github-button>
-                        <dark-mode-button></dark-mode-button>
-                        <ai-button></ai-button>
-                    </div>
-                </div>
+          
+           <header class="z-50 px-2 font-dm sticky top-4 left-0 ">
+                <nav class="flex py-3 justify-between items-center shadow-md  backdrop-blur-lg backdrop-brightness-125 dark:backdrop-brightness-110 
+                    text-gray-900 dark:shadow-purple-400 dark:text-gray-100 max-w-5xl rounded-full mx-auto">
+                     <div class="font-extrabold text-2xl w-1/6 px-5"><a href="/">Dota</a></div>
+                        <div class="md:flex justify-center items-center hidden w-4/6">
+                            <ul class="flex justify-between items-center gap-x-5 w-1/2">
+                                    ${this.items.map((item) => {
+                                        return `<li class="text-sm font-semibold hover:text-purple-600 text-gray-500 dark:text-gray-400">
+                                                  <a href="${item.url}">${item.name}</a>
+                                                 </li>`;
+                                    }).join("")}
+                             </ul>
+                          </div>
+                            <div class="flex items-center px-5 gap-x-3 justify-end">
+                            <github-button></github-button>
+                            <dark-mode-button></dark-mode-button>
+                             <ai-button></ai-button>
+                             <button class="md:hidden ">
+                             <dota-icon name="solar:hamburger-menu-linear" variant="ghost" size="md"></dota-icon>
+                               </button>
+                           </div>
+                </nav>
             </header>
         `;
   }

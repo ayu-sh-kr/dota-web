7a9fcdaea01367a6f1e10b69099bc42253d0d59c
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-22 22:33:13 +0530

    feat: refactor dark mode button component to utilize utility methods for theme handling

--- src/components/utils/buttons/dark-mode-button.component.ts
+++ src/components/utils/buttons/dark-mode-button.component.ts
@@ -1,8 +1,5 @@
-import {BaseElement, Component} from "@ayu-sh-kr/dota-core/dist";
-import {AfterInit, BindEvent} from "@ayu-sh-kr/dota-core";
-
-import {GeneralUtils, toggleDark} from "@dota/utils/GeneralUtils.ts";
-import {LocalStorageService} from "@dota/service/local-storage.service.ts";
+import {BaseElement, Component, BindEvent, WindowListener} from "@ayu-sh-kr/dota-core";
+import {GeneralUtils} from "@dota/utils/GeneralUtils.ts";
 
 
 @Component({
@@ -15,41 +12,23 @@ export class DarkModeButtonComponent extends BaseElement {
     super();
   }
 
-  @AfterInit()
-  afterViewInit() {
-    const browserTheme = GeneralUtils.getBrowserTheme();
-    switch (browserTheme) {
-      case 'dark':
-        this.querySelector('#dark-button')!.innerHTML = `<dota-icon name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`;
-        break;
-      case 'light':
-        this.querySelector('#dark-button')!.innerHTML = `<dota-icon name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`;
-        break;
-      default:
-        break;
-    }
-  }
-
   @BindEvent({event: 'click', id: '#dark-button'})
   handleDark() {
-    const iconContainer = this.querySelector('#dark-button');
+    GeneralUtils.toggleDarkMode();
+  }
 
-    if (iconContainer) {
-      if (toggleDark()) {
-        iconContainer.innerHTML = `<dota-icon  name="material-symbols:dark-mode" color="purple" variant="ghost" size="md" />`
-        LocalStorageService.add('theme', 'dark');
-      } else {
-        iconContainer.innerHTML = `<dota-icon  name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md" />`
-        LocalStorageService.add('theme', 'light');
-      }
-    }
+  @WindowListener({event: 'themeChange'})
+  handleThemeChange() {
+    this.updateHTML();
   }
 
   render(): string {
+    const isDarkTheme = GeneralUtils.isDarkMode();
+    const icon = isDarkTheme ? 'material-symbols:dark-mode' : 'material-symbols:sunny-rounded';
     // language=html
     return `
       <span id="dark-button" class="active:scale-95 cursor-pointer">
-        <dota-icon name="material-symbols:sunny-rounded" color="purple" variant="ghost" size="md"/>
+        <dota-icon name="${icon}" color="purple" variant="ghost" size="md"/>
       </span>
     `;
   }
